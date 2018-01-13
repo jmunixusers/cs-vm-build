997fb385f6a6b49ab60060ef89d9ba261e26fdb9
Kyle Laker <kyle@laker.email>
2018-01-12 21:44:01 -0500

    Add Settings and About dialogs
    Add a settings dialog so that users may specify the branch and URL to
    use without having to manually modify a file in their home directory.
    Additionally, provide an About dialog with information about the
    application. This provides two benefits:
     1. The user knows where the program came from and provides a link to
        our GitHub
     2. There's more than just one thing in the toolbar
    
    This also disables all the checkboxes when the Ansible script is
    running, which is a preliminary resolution to #109, but not a full
    enough solution to close that issue.
    
    Further, there is now a try-except around the Vte spawn call because if
    gksudo did not exist on the user's computer nothing at all would happen
    except for a message being logged or printed to the console. The buttons
    never got re-enabled because no call was ever made to
    sub_command_exited. Now the error gets logged and that function is
    called manually so a message can be displayed to the user.
    
    Closes #108

--- roles/task-shortcuts/files/uug_ansible_wrapper.py
+++ roles/task-shortcuts/files/uug_ansible_wrapper.py
@@ -17,8 +17,8 @@ import sys
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Vte', '2.91')
-from gi.repository import Gtk, Vte
-from gi.repository import GLib
+from gi.repository import Gtk, Vte # noqa
+from gi.repository import GLib     # noqa
 
 # If no tags are passed to ansible-pull, all of them will be run and I am
 # uncertain of the outcome of passing -t with no tags. To avoid this, always
@@ -62,15 +62,17 @@ def main():
         unable_to_detect_branch()
 
     # Show the window and ensure when it's closed that the script terminates
-    win = CheckboxWindow()
+    win = AnsibleWrapperWindow()
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
     Gtk.main()
 
 
-class CheckboxWindow(Gtk.Window):
+class AnsibleWrapperWindow(Gtk.Window):
     """The main window for the program. Includes a series of checkboxes for
        courses as well as a VTE to show the output of the Ansible command"""
+
+    checkboxes = []
 
     def __init__(self):
         Gtk.Window.__init__(self, title="JMU CS VM Configuration")
@@ -84,12 +86,14 @@ class CheckboxWindow(Gtk.Window):
         self.set_border_width(10)
 
         # Create a box to contain all elements that will be added to the window
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=12)
-        self.add(vbox)
+        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=12)
+        self.add(self.vbox)
+
+        self.create_toolbar()
 
         label = Gtk.Label("Select the courses you need configured on the VM")
         label.set_alignment(0.0, 0.0)
-        vbox.pack_start(label, False, False, 0)
+        self.vbox.pack_start(label, False, False, 0)
 
         courses_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         # This button doesn't do anything. Common is always run
@@ -105,15 +109,8 @@ class CheckboxWindow(Gtk.Window):
             checkbox.set_tooltip_text("Configure for %s" % course)
             checkbox.connect("toggled", self.on_button_toggled, tag)
             courses_box.pack_start(checkbox, False, False, 0)
-        vbox.pack_start(courses_box, False, False, 0)
-
-        # Add informational labels to the bottom of the window
-        label_box = Gtk.Box(spacing=6)
-        url_label = Gtk.Label("URL: %s" % CURRENT_CONFIG['URL'])
-        label_box.pack_start(url_label, False, False, 6)
-        branch_label = Gtk.Label("Branch: %s" % CURRENT_CONFIG['RELEASE'])
-        label_box.pack_start(branch_label, False, False, 6)
-        vbox.pack_end(label_box, False, False, 0)
+            self.checkboxes.append(checkbox)
+        self.vbox.pack_start(courses_box, False, False, 0)
 
         # Add run and cancel buttons
         button_box = Gtk.Box(spacing=6)
@@ -124,12 +121,12 @@ class CheckboxWindow(Gtk.Window):
         self.cancel_button = Gtk.Button.new_with_mnemonic("_Quit")
         self.cancel_button.connect("clicked", Gtk.main_quit)
         button_box.pack_end(self.cancel_button, True, True, 0)
-        vbox.pack_end(button_box, False, True, 0)
+        self.vbox.pack_end(button_box, False, True, 0)
 
         # Add the terminal to the window
         self.terminal = Vte.Terminal()
         self.terminal.connect("child-exited", self.sub_command_exited)
-        vbox.pack_end(self.terminal, True, True, 0)
+        self.vbox.pack_end(self.terminal, True, True, 0)
 
     @classmethod
     def on_button_toggled(cls, button, name):
@@ -140,11 +137,106 @@ class CheckboxWindow(Gtk.Window):
         else:
             TAGS.remove(name)
 
+    def create_toolbar(self):
+        menu_bar = Gtk.MenuBar()
+        file_menu = Gtk.Menu()
+        file_item = Gtk.MenuItem("File")
+        file_item.set_submenu(file_menu)
+
+        settings = Gtk.MenuItem("Settings\u2026")
+        settings.connect("activate", self.show_settings)
+        file_menu.append(settings)
+        quit = Gtk.MenuItem("Quit")
+        quit.connect("activate", Gtk.main_quit)
+        file_menu.append(quit)
+
+        menu_bar.append(file_item)
+
+        help_menu = Gtk.Menu()
+        help_item = Gtk.MenuItem("Help")
+        help_item.set_submenu(help_menu)
+
+        about = Gtk.MenuItem("About")
+        about.connect("activate", self.show_about_dialog)
+        help_menu.append(about)
+
+        menu_bar.append(help_item)
+        self.vbox.pack_start(menu_bar, False, False, 0)
+
+    def show_settings(self, _):
+        dialog = Gtk.Dialog(title="Settings", parent=self,
+                            flags=Gtk.DialogFlags.MODAL)
+        grid = Gtk.Grid()
+        branch_label = Gtk.Label("Branch:")
+        branch_label.set_justify(Gtk.Justification.RIGHT)
+        branch_label.set_halign(Gtk.Align.END)
+
+        url_label = Gtk.Label("URL:")
+        url_label.set_justify(Gtk.Justification.RIGHT)
+        url_label.set_halign(Gtk.Align.END)
+
+        branch_entry = Gtk.Entry()
+        url_entry = Gtk.Entry()
+        branch_entry.set_text(CURRENT_CONFIG['RELEASE'])
+        url_entry.set_text(CURRENT_CONFIG['URL'])
+        branch_entry.set_width_chars(40)
+        url_entry.set_width_chars(40)
+
+        grid.add(branch_label)
+        grid.attach_next_to(branch_entry, branch_label, Gtk.PositionType.RIGHT,
+                            1, 1)
+        grid.attach_next_to(url_label, branch_label, Gtk.PositionType.BOTTOM,
+                            1, 1)
+        grid.attach_next_to(url_entry, url_label, Gtk.PositionType.RIGHT, 1, 1)
+        dialog.get_content_area().pack_end(grid, False, False, 0)
+        dialog.add_button(Gtk.STOCK_OK, Gtk.ResponseType.OK)
+        dialog.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        dialog.set_default_size(400, 100)
+        grid.set_row_spacing(6)
+        grid.set_column_spacing(6)
+        grid.set_border_width(6)
+        grid.show_all()
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            CURRENT_CONFIG['RELEASE'] = branch_entry.get_text()
+            CURRENT_CONFIG['URL'] = url_entry.get_text()
+            write_config()
+        dialog.destroy()
+
+    def show_about_dialog(self, _):
+        about_dialog = Gtk.AboutDialog()
+        try:
+            self.set_logo('computer')
+        except Exception as err:
+            logging.error("Error", exc_info=err)
+        about_dialog.set_transient_for(self)
+        about_dialog.set_program_name("JMU CS VM Configuration")
+        about_dialog.set_copyright("Copyright \xa9 2018 JMU Unix Users"
+                                   " Group")
+        about_dialog.set_comments("A tool for configuring virtual machines"
+                                  " for use in the JMU Department of Computer"
+                                  " Science, maintained by the Unix Users"
+                                  " Group")
+        about_dialog.set_authors(["JMU Unix Users Group"])
+        about_dialog.set_website("https://github.com/jmunixusers/cs-vm-build")
+        about_dialog.set_website_label("Project GitHub page")
+        about_dialog.set_version("2.0")
+        about_dialog.set_license_type(Gtk.License.MIT_X11)
+        about_dialog.connect("response", self.on_close)
+        about_dialog.show()
+
+    def on_close(self, action, _):
+        action.destroy()
+
     def sub_command_exited(self, _, exit_status):
         """Displays a dialog informing the user whether the gksudo and
            ansible-pull commands completely successfully or not"""
+        for checkbox in self.checkboxes:
+            checkbox.set_sensitive(True)
         self.cancel_button.set_sensitive(True)
         self.run_button.set_sensitive(True)
+
         if exit_status == 0:
             success_msg = "Your machine has been configured for: %s" \
                           % (",".join(TAGS))
@@ -191,24 +283,32 @@ class CheckboxWindow(Gtk.Window):
                      "\nTo configure your virtual machine, administrator" \
                      " privileges are required."
 
+        for checkbox in self.checkboxes:
+            checkbox.set_sensitive(False)
+
         self.cancel_button.set_sensitive(False)
         self.run_button.set_sensitive(False)
 
         logging.info("Running ansible-pull with flags: %s", ",".join(TAGS))
 
-        self.terminal.spawn_sync(Vte.PtyFlags.DEFAULT,
-                                 os.environ['HOME'],
-                                 ["/usr/bin/gksudo", "--message",
-                                  gksudo_msg, "--",
-                                  "ansible-pull", "-U", CURRENT_CONFIG['URL'],
-                                  "-C", CURRENT_CONFIG['RELEASE'],
-                                  "--purge", "-i", "hosts",
-                                  "-t", ",".join(TAGS)],
-                                 [],
-                                 GLib.SpawnFlags.DO_NOT_REAP_CHILD,
-                                 None,
-                                 None,
-                                )
+        try:
+            self.terminal.spawn_sync(Vte.PtyFlags.DEFAULT,
+                                     os.environ['HOME'],
+                                     ["/usr/bin/gksudo", "--message",
+                                      gksudo_msg, "--",
+                                      "ansible-pull",
+                                      "-U", CURRENT_CONFIG['URL'],
+                                      "-C", CURRENT_CONFIG['RELEASE'],
+                                      "--purge", "-i", "hosts",
+                                      "-t", ",".join(TAGS)],
+                                     [],
+                                     GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                                     None,
+                                     None,
+                                     )
+        except GLib.Error as error:
+            logging.error("Unable to run ansible command.", exc_info=error)
+            self.sub_command_exited(None, 1)
 
 
 def show_dialog(parent, dialog_type, buttons_type, header, message):
@@ -353,6 +453,16 @@ def is_online():
     except OSError as err:
         logging.warning("%s is unreachable.", test_hostname, exc_info=err)
         return False
+
+
+def write_config():
+    """
+    Writes the user's configuration out to the configuration file. This
+    allows configuration changes to persist across invocations.
+    """
+    with open(USER_CONFIG_PATH, "w+") as config:
+        print("FORCE_GIT_URL = %s" % CURRENT_CONFIG['URL'], file=config)
+        print("FORCE_BRANCH = %s" % CURRENT_CONFIG['RELEASE'], file=config)
 
 
 if __name__ == "__main__":
